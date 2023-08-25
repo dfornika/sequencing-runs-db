544e033723e710dc1720a531d8c713c9d06a41f3
Dan Fornika <dfornika@gmail.com>
2023-08-24 17:03:39 -0700

    fix workflow

--- .github/scripts/install_flyway.sh
+++ .github/scripts/install_flyway.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/9.21.2/flyway-commandline-9.21.2-linux-x64.tar.gz
+wget -q https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/9.21.2/flyway-commandline-9.21.2-linux-x64.tar.gz
 tar -xzf flyway-commandline-9.21.2-linux-x64.tar.gz
 sudo ln -s `pwd`/flyway-9.21.2/flyway /usr/local/bin
