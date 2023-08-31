3245889f2bc6eec96f891975211886bcfa8ad874
Dan Fornika <dfornika@gmail.com>
2023-08-31 07:51:22 -0700

    Update install_flyway.sh

--- .github/scripts/install_flyway.sh
+++ .github/scripts/install_flyway.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -q https://download.red-gate.com/maven/release/org/flywaydb/enterprise/flyway-commandline/9.22.0/flyway-commandline-9.22.0-linux-x64.tar.gz
+wget -q https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/9.22.0/flyway-commandline-9.22.0-linux-x64.tar.gz
 tar -xzf flyway-commandline-9.22.0-linux-x64.tar.gz
 sudo ln -s `pwd`/flyway-9.22.0/flyway /usr/local/bin
