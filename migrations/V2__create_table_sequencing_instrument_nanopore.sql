f355ade416a9584fb8cc7a4ac660f807320d296d
Dan Fornika <dfornika@gmail.com>
2023-08-31 14:49:27 -0700

    Update V2__create_table_sequencing_instrument_nanopore.sql

--- migrations/V2__create_table_sequencing_instrument_nanopore.sql
+++ migrations/V2__create_table_sequencing_instrument_nanopore.sql
@@ -1,8 +1,7 @@
 CREATE TABLE sequencing_instrument_nanopore (
   pk SERIAL PRIMARY KEY,
   instrument_id VARCHAR(255) NOT NULL,
-  type VARCHAR(255),
-  model VARCHAR(255),
+  instrument_type VARCHAR(255),
   status VARCHAR(255),
   timestamp_status_updated TIMESTAMP
 );
