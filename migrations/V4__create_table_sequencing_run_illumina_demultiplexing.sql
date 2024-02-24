766504c448c632e5d5f63c5b0cc9574eda3577b7
Dan Fornika <dfornika@gmail.com>
2024-02-24 13:48:57 -0800

    Tighten up numeric types and constraints

--- migrations/V4__create_table_sequencing_run_illumina_demultiplexing.sql
+++ migrations/V4__create_table_sequencing_run_illumina_demultiplexing.sql
@@ -1,9 +1,10 @@
 CREATE TABLE sequencing_run_illumina_demultiplexing (
-  pk SERIAL PRIMARY KEY,
-  sequencing_run_illumina_fk INTEGER,
+  pk BIGSERIAL PRIMARY KEY,
+  sequencing_run_illumina_fk BIGINT NOT NULL,
   sequencing_run_id VARCHAR(255) NOT NULL,
   demultiplexing_id VARCHAR(255) NOT NULL,
   samplesheet_path VARCHAR(255),
   fastq_dir_path VARCHAR(255),
-  CONSTRAINT fk_sequencing_run_illumina FOREIGN KEY(sequencing_run_illumina_fk) REFERENCES sequencing_run_illumina(pk)
+  CONSTRAINT fk_sequencing_run_illumina FOREIGN KEY(sequencing_run_illumina_fk) REFERENCES sequencing_run_illumina(pk) ON DELETE CASCADE,
+  CONSTRAINT uq_sequencing_run_illumina_demultiplexing UNIQUE (sequencing_run_id, demultiplexing_id)
 );
